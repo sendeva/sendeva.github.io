6c9324101c03cecd97c7aab0f29e9628aec3c3b7
Sendy Deva Pastia <sendy.deva@corp.bri.co.id>
2019-09-09 17:44:24 +0700

    Service Worker Test

---
+++ js/sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(event) {
+    console.log('install sukses', event);
+    event.waitUntil(
+        caches.open('appku')
+        .then(function(cache) {
+            console.log('mulai caching');
+            return cache.addAll([
+                'index.html'
+            ]);
+        })
+        .then(function() {
+            self.skipWaiting();
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('aktivasi sukses', event);
+    self.skipWaiting();
+})
