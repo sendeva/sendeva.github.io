dbe4ffda00e085c8bca439b1ca0b445dbd7067de
Sendy Deva Pastia <sendy.deva@corp.bri.co.id>
2019-09-09 22:35:23 +0700

    sw update

--- js/sw.js
+++ js/sw.js
@@ -1,32 +1,32 @@
+const cacheVersion = 'v1-pwa-dasar';
+
+const filesToCache = [
+    '/',
+    '/css/sb-admin-2.min.css',
+    '/js/sb-admin-2.min.js',
+    '/vendor/jquery/jquery.min.js',
+    '/vendor/bootstrap/js/bootstrap.bundle.min.js"',
+    '/vendor/chart.js/Chart.min.js',
+    '/js/demo/chart-area-demo.js',
+    '/js/demo/chart-pie-demo.js',
+];
+
 self.addEventListener('install', function(event) {
-    console.log('install sukses', event);
-    event.waitUntil(
-        caches.open('appku')
-        .then(function(cache) {
-            console.log('mulai caching');
-            return cache.addAll([
-                '/index.html',
-                '/buttons.html'
-
-            ]);
-        })
-        .then(function() {
-            self.skipWaiting();
-        })
-    );
-});
-
-self.addEventListener('activate', function(event) {
-    console.log('aktivasi sukses', event);
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(cacheVersion)
+      .then(function(cache) {
+        return cache.addAll(filesToCache)
+      })
+  )
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(res) {
-            if (res) return res;
-            return fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) return res;
+
+        return fetch(event.request);
+      })
+  );
 });
